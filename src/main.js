40cdae8afd56d96bbc6bcb081cda42ca2734432e
Mikkel Malmberg <mikkel@brnbw.com>
2020-04-25 08:24:47 +0200

    Initial

---
+++ src/main.js
@@ -0,0 +1,3 @@
+import 'prismjs'
+import './prism-nord.css'
+import 'alpinejs'
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        "spr-pink": "var(--spr-pink)",
+        "spr-blue": "var(--spr-blue)",
+        "spr-mint": "var(--spr-mint)",
+      },
+    },
+  },
+};
