01c41aed1957a9dbc3707eff2431c6e2912f02c2
Mikkel Malmberg <mikkel@brnbw.com>
2020-04-27 23:20:21 +0200

    Fix build

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,16 @@
-const plugins = [require("tailwindcss")];
+const plugins = [
+  require("tailwindcss"),
+  require("postcss-preset-env")({
+    autoprefixer: { flexbox: "no-2009" },
+    stage: 1
+  }),
+];
 
 if (process.env.NODE_ENV === "production") {
   plugins.push(
     require("@fullhuman/postcss-purgecss")({
-      content: ["./public/index.html"],
-    })
-  );
-  plugins.push(
-    require("cssnano")({
-      preset: ["default", { discardComments: { removeAll: true } }],
+      content: ["./src/index.html"],
+      defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
     })
   );
 }
